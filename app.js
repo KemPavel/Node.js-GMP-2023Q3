03a5c62479ef8e3bc96572dcca7da2a0b65fd100
KemPavel <pkazakov18@gmail.com>
2023-09-09 16:34:12 +0300

    Module 2 - Standard Library

---
+++ app.js
@@ -0,0 +1,39 @@
+const os = require('os');
+const fs = require('fs');
+const childProcess = require('child_process');
+
+const commands = {
+  unix: 'ps -A -o %cpu,%mem,comm | sort -nr | head -n 1',
+  windows: 'powershell "Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + \' \' + $_.CPU + \' \' + $_.WorkingSet }"'
+};
+
+const command = os.platform() === 'win32' ? commands.windows : commands.unix;
+
+const saveData = (data) => {
+  setInterval(() => {
+    const time = new Date().getTime();
+    const newLine = `${time}: ${data}`;
+    fs.appendFile('activityMonitor.log', newLine, (err) => {
+      if (err) throw err;
+      console.warn('Updated!');
+    });
+  }, 60 * 1000);
+};
+
+const execProcess = (cmd) => {
+  childProcess.exec(cmd, (error, stdout) => {
+    console.clear();
+    console.log(stdout);
+    saveData(stdout);
+  });
+};
+
+setInterval(() => {
+  execProcess(command);
+}, 100);
+
+process.on('uncaughtException', (error) => {
+  console.error(`${(new Date).toUTCString()} uncaught exception: ${error.message}`);
+  console.error(error.stack);
+  process.exit(1);
+});
